a9dafd8a8712cc6004feed9cb0bfc4772dd8b9e7
Даниил Овсянников <ovsuanikov@gmail.com>
2021-05-24 15:36:53 +0300

    Fix #1

--- js/task-05.js
+++ js/task-05.js
@@ -2,7 +2,7 @@ const inputEl = document.getElementById(`name-input`);
 const spanEl = document.getElementById(`name-output`);
 inputEl.addEventListener(`input`, changeOutput);
 function changeOutput(event) {
-    if (event.currentTarget.value === ``)
+    if (event.currentTarget.value === `` || event.currentTarget.value.trim() === ``)
     { spanEl.textContent = `незнакомец` }
     else {
         spanEl.textContent = event.currentTarget.value
